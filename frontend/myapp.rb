7890c9cb10d88ade9e42124c23952448768f59a5
nstehr <none@none>
2010-11-29 22:33:18 -0500

    disqus comment system added, plus some simple stats collection

--- frontend/myapp.rb
+++ frontend/myapp.rb
@@ -2,30 +2,48 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 require 'dm-core'
-
+require 'open-uri'
 
 @config = JSON.parse(File.read('config.json'))
-$index = @config['static']
+@static = @config['static']
 $upload = @config['upload']
 puts $upload
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db.sqlite3")
-set :public, @index
+set :public, @static
 set:port, 12599
 class Song
   include DataMapper::Resource
-  
   property :id, Serial
   property :name, String
   property :path, String
   property :queued_at, DateTime
   property :is_playing, Boolean, :default  => false
   property :is_queued, Boolean, :default  => false
+  property :play_count, Integer, :default => 0
+end
+
+class CityStat
+  include DataMapper::Resource
+  property :id, Serial
+  property :city, String
+  property :submit_count, Integer, :default => 0
+end
+
+class TimeStat
+  include DataMapper::Resource
+  property :id, Serial
+  property :submit_time, Integer
+  property :submit_count, Integer, :default => 0
 end
 
 DataMapper.auto_upgrade!
 
 get '/' do
-  send_file('index.html')
+  send_file('static/index.html')
+end
+
+get '/stats' do
+  send_file('static/stats.html')
 end
 
 get '/now_playing' do
@@ -72,6 +90,7 @@ get '/track_done' do
    song = Song.first(:is_playing=>true)
     if song != nil
     song.is_playing = false
+    song.play_count = song.play_count + 1
     song.save
   end
 end
@@ -83,6 +102,8 @@ post '/upload' do
     FileUtils.mv(params[:file][:tempfile].path, "#{$upload}/#{params[:file][:filename]}")
     song = Song.new(:name => params[:name], :path => "#{$upload}/#{params[:file][:filename]}",:queued_at=>Time.now,:is_queued=>true)
     song.save
+    save_stats("99.246.119.119")
+    #save_stats(@env['REMOTE_ADDR'])
   end
   redirect(back())
 end
@@ -92,7 +113,81 @@ post '/queue_song' do
    song.queued_at = Time.now
    song.is_queued= true
    song.save
+   save_stats("99.246.119.119")
+   #save_stats(@env['REMOTE_ADDR'])
 end 
+
+get '/top_songs' do 
+   songs = Song.all(:limit=>5,:play_count.gt=>0,:order => [:play_count.desc])
+   song_list = []
+   songs.each do |song|
+   song_list = song_list.push({:name => song.name})
+   end
+   content_type:json
+     song_list.to_json
+end
+
+get '/song_stats' do 
+   songs = Song.all(:play_count.gt=>0)
+   song_list = []
+   songs.each do |song|
+   song_list = song_list.push({:name => song.name,:play_count => song.play_count})
+   end
+   content_type:json
+     song_list.to_json
+end
+
+get '/time_stats' do
+  stats = TimeStat.all(:submit_count.gt=>0)
+  stat_list = []
+  stats.each do |stat|
+    stat_list = stat_list.push({:submit_time => stat.submit_time,:submit_count=>stat.submit_count})
+  end
+  content_type:json
+     stat_list.to_json
+end
+
+get '/city_stats' do
+  stats = CityStat.all(:submit_count.gt=>0)
+  stat_list = []
+  stats.each do |stat|
+    stat_list = stat_list.push({:city => stat.city,:submit_count=>stat.submit_count})
+  end
+  content_type:json
+     stat_list.to_json
+end
+
+def save_stats(ip)
+    #collect where the submitter is from
+    contents = open("http://www.geoplugin.net/json.gp?ip=#{ip}") {|io| io.read}
+    contents.slice!("geoPlugin(")
+    contents.slice!(")")
+    #save the city
+    city = JSON.parse(contents)['geoplugin_city']
+    city_stat = CityStat.first(:city => city)
+    puts city_stat
+    if city_stat == nil
+      city_stat = CityStat.new(:city=>city,:submit_count=>1)
+    else
+      city_stat.submit_count = city_stat.submit_count + 1
+    end
+    city_stat.save
+    #save the time
+    time = Time.new
+    #get the the EST time
+    time = time.utc - (5 * 60 * 60)
+    current_hour = time.hour
+    time_stat = TimeStat.first(:submit_time => current_hour)
+    if time_stat == nil
+      time_stat = TimeStat.new(:submit_time => current_hour, :submit_count => 1)
+    else
+      time_stat.submit_count = time_stat.submit_count + 1
+    end
+    time_stat.save
+    
+end
+
+
 =begin
 get '/delete/song/:name' do |n|
   song = Song.first(:name=>n)
---
+++ frontend/static/frontend.js
@@ -0,0 +1,97 @@
+  // You may specify partial version numbers, such as "1" or "1.3",
+  //  with the same result. Doing so will automatically load the 
+  //  latest version matching that partial revision pattern 
+  //  (i.e. both 1 and 1.3 would load 1.3.2 today).
+  google.load("jquery", "1.4.2");
+
+  google.setOnLoadCallback(function() {
+    // Place init code here instead of $(document).ready()
+	fillSongOptions()
+	getQueuedSongs()
+	getNowPlaying()
+	getTopSongs()
+	var refreshId = setInterval(function() {
+		getQueuedSongs()
+		getNowPlaying()
+		getTopSongs()
+	}, 3000);
+	
+	
+	$('#selectSubmit').click(function(){
+		
+		var dataString = "value="+$('#songOptions').val()
+		
+		$.ajax({
+		    url:"queue_song",
+			type: "POST",
+		    data: dataString,
+		    })
+		
+		getQueuedSongs()
+	})
+	
+	
+});
+
+function fillSongOptions(){
+//load all the songs via json
+
+$.getJSON("all_songs",
+        function(data){
+			$.each(data, function(i,item){
+				
+            	$('#songOptions').append(
+				        $('<option></option>').val(item.id).html(item.name)
+				    )
+          })
+        })
+
+}
+
+function getQueuedSongs(){
+//load all the songs via json
+
+$.getJSON("queued_songs",
+        function(data){
+			$('#queueList').find('li').remove()
+			$.each(data, function(i,item){
+				
+            	$('#queueList').append(
+				        $('<li></li>').html(item.name)
+				    )
+          })
+        })
+
+}
+
+function getNowPlaying(){
+$.getJSON("now_playing",
+        
+		function(data){
+		
+		$(".nowPlaying").html(data.name)
+        })
+}
+
+function getTopSongs(){
+    $.getJSON("top_songs",
+            function(data){
+    			$('#topList').find('li').remove()
+    			$.each(data, function(i,item){
+
+                	$('#topList').append(
+    				        $('<li></li>').html(item.name)
+    				    )
+              })
+            })
+}
+
+function validateFileExtension(fld) {
+if(!/(\.mid|\.midi|\.MID|\.MIDI)$/i.test(fld.value)) {
+	alert("Invalid file type.");
+	fld.form.reset();
+	fld.focus();
+	return false;
+}
+return true;
+}
